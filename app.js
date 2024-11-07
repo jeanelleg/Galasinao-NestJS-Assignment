9456bd0775ebc6d46df53ba8e851fd531899c81e
John Philip G. Cauilan <jeanelle.galasinao@cvsu.edu.ph>
2024-11-08 00:27:38 +0800

    Assignment-3-Factorial-Calculator

---
+++ app.js
@@ -0,0 +1,13 @@
+
+
+const express = require('express');
+const app = express();
+const assignmentRoutes = require('./routes/ass3routes'); 
+
+
+app.use('/assignments', assignmentRoutes);
+
+const PORT = 9090;  
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
---
+++ routes/ass3routes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const { getFactorial } = require('../controller/ass3controller');
+
+
+router.get('/factorial/:number', (req, res) => {
+    const number = parseInt(req.params.number);
+    const result = getFactorial(number);
+
+    if (result.error) {
+        return res.status(400).json(result);
+    }
+
+    res.json(result);
+});
+
+module.exports = router;
